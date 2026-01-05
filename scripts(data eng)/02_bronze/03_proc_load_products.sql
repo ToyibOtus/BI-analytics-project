c365130277cfc8d202dc79f3b7ea8ca1e668ab14
Toyib Otusanya <toyibotusanya@gmail.com>
2026-01-05 15:07:50 +0000

    Update 03_proc_load_products.sql

--- scripts(data eng)/02_bronze/03_proc_load_products.sql
+++ scripts(data eng)/02_bronze/03_proc_load_products.sql
@@ -28,7 +28,7 @@ BEGIN
 	DECLARE
 	@step_run_id INT,
 	@step_name NVARCHAR(50) = 'usp_load_bronze_products',
-	@load_type NVARCHAR(50) = 'Full',
+	@load_type NVARCHAR(50) = 'FULL',
 	@ingest_layer NVARCHAR(50) = 'BRONZE',
 	@ingest_table NVARCHAR(50) = 'products',
 	@start_time DATETIME,
