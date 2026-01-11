19969bab1c067b8279a1c71bbe353ddca067e7fa
Toyib Otusanya <toyibotusanya@gmail.com>
2026-01-11 12:18:43 +0000

    Create 04_test_run_procs.sql

---
+++ scripts(data eng)/03_silver/04_test_run_procs.sql
@@ -0,0 +1,38 @@
+/*
+========================================================================
+Ad hoc: Test bronze procedures
+========================================================================
+Script Purpose:
+	This procedure tests the working conditions of all silver procedures
+	in [SalesDatabase]. It firstly loads into metadata.etl_job_run, 
+	satisfying the foreign key constraint set on dependent tables. 
+========================================================================
+*/
+USE SalesDatabase;
+GO
+
+-- Create variable
+DECLARE @job_run_id INT;
+
+-- Load data into log table
+INSERT INTO metadata.etl_job_run
+(
+	job_name,
+	job_scope,
+	start_time,
+	job_status
+)
+VALUES
+(
+	'dev_test_bronze_run',
+	'SILVER_TEST',
+	GETDATE(),
+	'TEST_RUN'
+);
+-- Retrieve newly generated job_run_id from log table
+SET @job_run_id = SCOPE_IDENTITY();
+
+-- Load Silver Layer
+EXEC silver.usp_load_silver_customers @job_run_id;
+EXEC silver.usp_load_silver_orders @job_run_id;
+EXEC silver.usp_load_silver_products @job_run_id;
