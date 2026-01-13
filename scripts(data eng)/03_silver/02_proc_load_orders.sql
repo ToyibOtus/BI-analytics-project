a36e67a4ab3230ea1bf4aad81f71b02629ad317d
Toyib Otusanya <toyibotusanya@gmail.com>
2026-01-13 12:54:16 +0000

    Update 02_proc_load_orders.sql

--- scripts(data eng)/03_silver/02_proc_load_orders.sql
+++ scripts(data eng)/03_silver/02_proc_load_orders.sql
@@ -27,9 +27,9 @@ BEGIN
 	-- Create and map values to variables where necessary
 	DECLARE
 	@step_run_id INT,
-	@step_name NVARCHAR(50) = 'usp_load_silver_orders',
+	@step_name NVARCHAR(50) = 'silver.usp_load_silver_orders',
 	@load_type NVARCHAR(50) = 'INCREMENTAL',
-	@ingest_layer NVARCHAR(50) = 'SILVER',
+	@ingest_layer NVARCHAR(50) = 'silver',
 	@ingest_table NVARCHAR(50) = 'orders',
 	@start_time DATETIME,
 	@end_time DATETIME,
@@ -358,7 +358,7 @@ BEGIN
 			WHERE step_run_id = @step_run_id;
 
 		-- Load error details into log table
-		INSERT INTO metadata.etl_error_log
+		INSERT INTO metadata.etl_step_run_error_log
 		(
 			job_run_id,
 			step_run_id,
