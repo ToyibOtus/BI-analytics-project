38f6786435aad9aba2fe62a47190a8c455d552fb
Toyib Otusanya <toyibotusanya@gmail.com>
2026-01-23 16:43:27 +0000

    Update 01_proc_load_customers.sql

--- scripts(data eng)/02_bronze/01_proc_load_customers.sql
+++ scripts(data eng)/02_bronze/01_proc_load_customers.sql
@@ -14,7 +14,7 @@ Usage: EXEC bronze.usp_load_bronze_customers @job_run_id
 Note:
 	* Running this script independently demands that you assign an integer value to @job_run_id.
 	* It is imperative that this value already exist in the log table [metadata.etl_job_run] due
-	  to the foreign key constraint set on log table [metadata.etl_step_run].
+	  to the foreign key constraint set on dependent tables.
 	* To test the working condition of this script, check folder titled "test_run".
 =============================================================================================
 */
