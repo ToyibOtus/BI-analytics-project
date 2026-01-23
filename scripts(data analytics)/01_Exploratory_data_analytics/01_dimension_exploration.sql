42d1437379c00c39822bc89ab4cf4b3871874744
Toyib Otusanya <toyibotusanya@gmail.com>
2026-01-23 16:35:43 +0000

    Update 01_dimension_exploration.sql

--- scripts(data analytics)/01_Exploratory_data_analytics/01_dimension_exploration.sql
+++ scripts(data analytics)/01_Exploratory_data_analytics/01_dimension_exploration.sql
@@ -1,12 +1,15 @@
 /*
-====================================================================================
+=============================================================================
 Dimension Exploration
-====================================================================================
+=============================================================================
 Script Purpose:
-	This script explores dimensions in relevant datasets.
-====================================================================================
+	This script explores relevant dimensions in gold layer, with the purpose
+	of getting some level of familiarity with the business.
+=============================================================================
 */
+-- Explore geographical locations/hierarchy
 SELECT DISTINCT country, city, postal_code FROM gold.dim_customers;
 
+-- Explore product hierarchy
 SELECT DISTINCT category, sub_category, product_name FROM gold.dim_products
 ORDER BY category, sub_category, product_name;
